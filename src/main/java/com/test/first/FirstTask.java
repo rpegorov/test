d6c0fc0588894272f585a5e7b0f18b6938aa2be6
rpegorov <rostislav.egorov@gmail.com>
2021-03-22 10:34:08 +0300

    регистронезависимый поиск в коллекции строк

--- src/main/java/com/test/first/FirstTask.java
+++ src/main/java/com/test/first/FirstTask.java
@@ -3,13 +3,14 @@ package com.test.first;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Дописать метод поиска в коллекци строк все строки, которые на java регистроНЕзависимо
  */
 public class FirstTask {
 
-    private static final List<String> STRINGS = Arrays.asList("Java", "java", "First element", "Last java");
+    private static final List<String> strings = Arrays.asList("Java", "java", "First element", "Last java");
 
     public static void main(String[] args) {
         List<String> result = getAllJava();
@@ -20,6 +21,8 @@ public class FirstTask {
      * Дописать метод
      */
     private static List<String> getAllJava() {
-        return new ArrayList<>();
+        return strings.stream()
+                .filter(w -> w.equalsIgnoreCase("java"))
+                .collect(Collectors.toList());
     }
 }
