93c446dcf27a44f7e089c94f6266765f835e2f3d
rpegorov <rostislav.egorov@gmail.com>
2021-03-22 12:07:33 +0300

    Сортировка массива чисел

--- src/main/java/com/test/four/FourTask.java
+++ src/main/java/com/test/four/FourTask.java
@@ -8,12 +8,27 @@ import java.util.Arrays;
 public class FourTask {
 
     public static void main(String[] args) {
-        int[] values = new int[] {4, 1, 2, 5, 9};
+        int[] values = new int[]{4, 1, 2, 5, 9};
         int[] sorted = sort(values);
         System.out.println(Arrays.toString(sorted));
     }
 
     private static int[] sort(int[] arr) {
-        return new int[] {0};
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int minI = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minI = j;
+                }
+            }
+            if (i != minI) {
+                int temp = arr[i];
+                arr[i] = arr[minI];
+                arr[minI] = temp;
+            }
+        }
+        return arr;
     }
 }
