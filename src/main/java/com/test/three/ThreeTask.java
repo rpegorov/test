ff8a744cf3effab2dd185a1d2b5396a865e94cd7
rpegorov <rostislav.egorov@gmail.com>
2021-03-22 12:13:12 +0300

    Сумма элементов массива

--- src/main/java/com/test/three/ThreeTask.java
+++ src/main/java/com/test/three/ThreeTask.java
@@ -6,12 +6,16 @@ package com.test.three;
 public class ThreeTask {
 
     public static void main(String[] args) {
-        int[] values = new int[] {1, 2, 3, 4, 5};
+        int[] values = new int[]{1, 2, 3, 4, 5};
         int result = sum(values);
         System.out.println(result);
     }
 
     private static int sum(int[] arr) {
-        return 0;
+        int sum = 0;
+        for (int j : arr) {
+            sum = sum + j;
+        }
+        return sum;
     }
 }
